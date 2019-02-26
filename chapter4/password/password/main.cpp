21984cfe3136f1ea2cd3c3a18f0bf5a775d3a5ff
Brian Lobo <brianlobo.code@gmail.com>
2019-02-26 10:27:02 -0500

    Updating pw assigment and runningTheRace

--- chapter4/password/password/main.cpp
+++ chapter4/password/password/main.cpp
@@ -6,16 +6,23 @@ using namespace std;
 int main(){
     const string PASSWORD = "P12345!";
     string userPass;
+    int i = 1;
     
     cout << "Please enter password: ";
     cin >> userPass;
+
+    while (userPass != PASSWORD && i < 3)
+    {
+        cout << "\tAttempts left: " << (3 - i++) << endl;
+        cout << "\tInvalid Password\n\n";
+        cout << "Please enter password: ";
+        cin >> userPass;
+    }
     
     if (userPass == PASSWORD)
-        cout << "Correct\n";
+        cout << "\tAccess granted!\n\n";
     else
-        cout << "Invalid\n";
-    
-    //cout << ((userPass == PASSWORD) ? "Correct\n" : "Invalid\n");
+        cout << "\tPleas contact your bank.\n\n";
     
     return 0;
 }
