28e32630ef0ee5759eb5af080762f717920123d4
Brian Lobo <brianlobo.code@gmail.com>
2019-02-26 07:37:41 -0500

    Finished assignment

--- chapter4/runningTheRace/runningTheRace/main.cpp
+++ chapter4/runningTheRace/runningTheRace/main.cpp
@@ -1,51 +1,96 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
-
 using namespace std;
 
-int main() {
-    float time1, time2, time3, high, mid, low;
-    string name1, name2, name3;
+int main()
+{
+    string runner1, runner2, runner3, first, second, third;
+    float runTime1, runTime2, runTime3;
     
-    cout << "Enter name 1: ";
-    getline(cin, name1);
-    cout << "Enter time 1: ";
-    cin >> time1;
-    if (time1 < 0)
-        cout << "\n\nPositive numbers only\n\n";
+    // First Runner
+    cout << "\n\n";
+    cout << "Fist runner: ";
+    getline(cin, runner1);
+    cout << "Time for " << runner1 << " take to finish the race: ";
+    cin  >> runTime1;
+    if (runTime1 < 0)
+        cout << "\nError: Time can't be negative.\n\n";
     else
     {
-        high = time1;
-        
-        cout << "Enter name 2: ";
-        getline(cin, name2);
-        cout << "Enter time 2: ";
-        cin >> time2;
-        if (time2 < 0)
-            cout << "\n\nPositive numbers only\n\n";
+        cin.ignore();
+        // Second Runner
+        cout << "What is the name of the second runner? ";
+        getline(cin, runner2);
+        cout << "Time for " << runner2 << " take to finish the race: ";
+        cin  >> runTime2;
+        if (runTime2 < 0)
+            cout << "\nError: Time can't be negative.\n\n";
         else
         {
-            if (time2 > high)
-            {
-                mid = high;
-                high = time2;
-            }
-            else
-                mid = time2;
-                
-            cout << "Enter name 3: ";
-            getline(cin, name3);
-            cout << "Enter time 3: ";
-            cin >> time3;
-            if (time3 < 0)
-                cout << "\n\nPositive numbers only\n\n";
+            cin.ignore();
+            // Third Runner
+            cout << "What is the name of the third runner? ";
+            getline(cin, runner3);
+            cout << "Time for " << runner3 << " take to finish the race: ";
+            cin  >> runTime3;
+            if (runTime3 < 0)
+                cout << "\nError: Time can't be negative.\n\n";
             else
             {
-                if (time3 > b  )
+                if (runTime1 > 0 && runTime2 > 0 && runTime3 > 0)
+                {
+                    if (runTime1 < runTime2 && runTime1 < runTime3)
+                    {
+                        if (runTime2 < runTime3)
+                        {
+                            first = runner1;
+                            second = runner2;
+                            third = runner3;
+                        }
+                        else
+                        {
+                            first = runner1;
+                            second = runner3;
+                            third = runner2;
+                        }
+                    }
+                    else if (runTime2 < runTime1 && runTime2 < runTime3)
+                    {
+                        if (runTime1 < runTime3)
+                        {
+                            first = runner2;
+                            second = runner1;
+                            third = runner3;
+                        }
+                        else
+                        {
+                            first = runner2;
+                            second = runner3;
+                            third = runner1;
+                        }
+                    }
+                    else
+                    {
+                        if (runTime1 < runTime2)
+                        {
+                            first = runner3;
+                            second = runner1;
+                            third = runner2;
+                        }
+                        else
+                        {
+                            first = runner3;
+                            second = runner2;
+                            third = runner1;
+                        }
+                    }
+                }
+                cout << setw(11) << left << "\n\tFirst: " << first << endl;
+                cout << setw(10) << left << "\tSecond: " << second << endl;
+                cout << setw(10) << left << "\tThird: " << third << "\n\n";
             }
         }
     }
-        
-    
     return 0;
 }
