fd73d8cbe488882d3d425a92051415f62c26f21d
Brian Lobo <brianlobo.code@gmail.com>
2019-03-07 22:37:16 -0500

    added code to the top of the code

--- chapter4/shippingCharge/shippingCharge/main.cpp
+++ chapter4/shippingCharge/shippingCharge/main.cpp
@@ -1,3 +1,22 @@
+//    15.
+//        Shipping Charges
+//
+//            The Fast Freight Shipping Company charges the following rates:
+//
+//        Weight of Package (in Kilograms) Rate per 500 Miles Shipped
+//            2 kg or less $1.10
+//            Over 2 kg but not more than 6 kg $2.20
+//            Over 6 kg but not more than 10 kg $3.70
+//            Over 10 kg but not more than 20 kg $4.80
+//
+//            Write a program that asks for the weight of the package and the distance it is to be
+//        shipped, and then displays the charges.
+//
+//            Input Validation: Do not accept values of 0 or less for the weight of the package. Do
+//        not accept weights of more than 20 kg (this is the maximum weight the company will
+//        ship). Do not accept distances of less than 10 miles or more than 3,000 miles. These
+//        are the company’s minimum and maximum shipping distances
+
 #include <iostream>
 
 using namespace std;
@@ -6,13 +25,13 @@ int main() {
     float rate, shippingCharge;
     int weight, distance;
     
-    cout << "Enter weight: ";
+    cout << "Enter weight (kg): ";
     cin >> weight;
     if (weight <= 0 || weight > 20)
         cout << "Enter Valid Weight" << endl;
     else
     {
-        cout << "Enter distance: ";
+        cout << "Enter distance (miles): ";
         cin >> distance;
         if (distance < 10 || distance > 3000)
             cout << "Enter Valid Distance" << endl;
@@ -32,7 +51,7 @@ int main() {
             if (distance % 500 > 0)
                 shippingCharge += rate;
             
-            //DISPLAY SHIPPING CHARGE HERE
+            cout << "\nShipping Charge: " << shippingCharge << "\n\n";
         }
     }
     return 0;
